bd9a5e1569bd5b83d34d41546f3ffa72d216e855
james-desktop <ninjachimpstudios@gmail.com>
2023-03-30 12:39:45 +1000

    init UIToolkit

---
+++ Assets/scripts/managers/SimManager.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using ncs.utils;
+using UnityEngine;
+
+namespace ncs {
+
+    public class SimManager : Singleton<SimManager> {
+        
+    }
+
+}
---
+++ Assets/scripts/managers/UIManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using ncs.utils;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+namespace ncs {
+
+    public class UIManager : Singleton<UIManager> {
+
+        private VisualTreeAsset _visualTree;
+        private UIDocument _uiDocument;
+
+        void Start() {
+            _uiDocument = gameObject.GetComponent<UIDocument>();
+            StartCoroutine(OneSecondUpdate());
+        }
+
+        private IEnumerator OneSecondUpdate() {
+            while (true) {
+                yield return new WaitForSeconds(1);
+            }
+        }
+
+    }
+
+}
