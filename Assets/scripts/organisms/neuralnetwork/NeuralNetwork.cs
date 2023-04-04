0e7df6631ab19f1f18b8c66be28cdb1b6c59c673
james-desktop <ninjachimpstudios@gmail.com>
2023-04-04 21:30:09 +1000

    initial NN

---
+++ Assets/scripts/organisms/neuralnetwork/NeuralNetwork.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using UnityEngine;
+
+namespace ncs.neuralnetwork {
+
+    public class NeuralNetwork : MonoBehaviour {
+
+        public Layer Input;
+        public Layer Hidden1;
+        public Layer Hidden2;
+        public Layer Output;
+        
+        public class Layer {
+
+            public float[,] WeightsArray;
+            public float[] BiasesArray;
+            public float[] NodeArray;
+
+            private readonly int nNodes;
+            private readonly int nInputs;
+
+            public Layer(int inputs, int nodes) {
+                nInputs = inputs;
+                nNodes = nodes;
+                WeightsArray = new float[nNodes, nInputs];
+                BiasesArray = new float[nNodes];
+                NodeArray = new float[nNodes];
+
+            }
+
+        }
+
+        public void Awake() {
+            
+        }
+
+    }
+
+}
