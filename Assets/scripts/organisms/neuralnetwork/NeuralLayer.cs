8f3795094b16f40ad198a03469915aa188c94ce8
james-desktop <ninjachimpstudios@gmail.com>
2023-04-05 15:35:02 +1000

    update to NN

---
+++ Assets/scripts/organisms/neuralnetwork/NeuralLayer.cs
@@ -0,0 +1,39 @@
+﻿namespace ncs.neuralnetwork {
+
+    public class NeuralLayer {
+
+        public float[,] WeightsArray;
+        public float[] BiasesArray;
+        public float[] NodeArray;
+
+        private readonly int nNodes;
+        private readonly int nInputs;
+
+        public NeuralLayer(int inputs, int nodes) {
+            nInputs = inputs;
+            nNodes = nodes;
+            WeightsArray = new float[nNodes, nInputs];
+            BiasesArray = new float[nNodes];
+            NodeArray = new float[nNodes];
+
+        }
+
+        public void Forward(float[] inputsArray) {
+            for (int i = 0; i < nNodes; i++) {
+                for (int j = 0; j < nInputs; j++) {
+                    NodeArray[i] += WeightsArray[i, j] * inputsArray[j];
+                }
+
+                NodeArray[i] += BiasesArray[i];
+            }
+        }
+
+        public void Activation() {
+            for (int i = 0; i < nNodes; i++) {
+                if (NodeArray[i] < 0) NodeArray[i] = 0;
+            }
+        }
+
+    }
+
+}
---
+++ Assets/scripts/organisms/neuralnetwork/NeuralNetworkMono.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using UnityEditor;
+using UnityEngine;
+
+namespace ncs.neuralnetwork {
+
+    public class NeuralNetworkMono : MonoBehaviour {
+
+        public NeuralLayer[] Layers;
+        [SerializeField] public int[] networkShape = { 2, 4, 4, 2 };
+
+        public void Awake() {
+            Layers = new NeuralLayer[networkShape.Length - 1];
+            for (int i = 0; i < Layers.Length; i++) {
+                Layers[i] = new NeuralLayer(networkShape[i], networkShape[i + 1]);
+            }
+        }
+
+        public float[] Brain(float[] inputs) {
+            for (int i = 0; i < Layers.Length; i++) {
+                if (i == 0) {
+                    // first hidden layer driven by inputs 
+                    Layers[i].Forward(inputs);
+                    Layers[i].Activation();
+                } else if (i == Layers.Length - 1) {
+                    // output layer, no activation
+                    Layers[i].Forward(Layers[i - 1].NodeArray);
+                } else {
+                    // other hidden layers
+                    Layers[i].Forward(Layers[i - 1].NodeArray);
+                    Layers[i].Activation();
+                }
+            }
+
+            return Layers[^1].NodeArray;
+        }
+
+    }
+
+}
